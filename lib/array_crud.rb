3e7a1f9b83dfa9b77a1d5e54e7bef6a7bf4f4114
Renée Cruz <reneebasscruz@gmail.com>
2019-03-20 20:01:36 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  array.new = ["cat","guinea pig","dog","cow"]
+  Array.new = ["cat","guinea pig","dog","cow"]
 end
 
 def add_element_to_end_of_array(array, element)
